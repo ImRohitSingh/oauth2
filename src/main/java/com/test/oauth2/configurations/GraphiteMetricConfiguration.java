b104fd2f58456cd83791247120945fe82991a567
Rohit Singh <Rohit.Singh@cerner.com>
2021-05-21 18:03:50 +0530

    Changing deprecated compile module with implementation in build.gradle

--- src/main/java/com/test/oauth2/configurations/GraphiteMetricConfiguration.java
+++ src/main/java/com/test/oauth2/configurations/GraphiteMetricConfiguration.java
@@ -1,5 +1,6 @@
-package com.test.oauth2.configuration;
+package com.test.oauth2.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +9,16 @@ import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
 public class GraphiteMetricConfiguration {
-	
+
+	@Value("${management.metrics.export.graphite.tags-as-prefix}")
+	private String commonTag;
+
+	@Value("${oauth2.graphitedb.metric-name}")
+	private String graphiteDbMetricName;
+
 	@Bean
 	public MeterRegistryCustomizer<MeterRegistry> commonTags() {
-	   return r -> r.config().commonTags("application", "oauth2");
+		return r -> r.config().commonTags(commonTag, graphiteDbMetricName);
 	}
 
 }
