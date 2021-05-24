64b5ff1ca57b950d3e4dda98b74aa63879dafb55
Rohit Singh <Rohit.Singh@cerner.com>
2021-05-24 21:42:47 +0530

    Commenting https config

--- src/main/java/com/test/oauth2/configurations/AppConfiguration.java
+++ src/main/java/com/test/oauth2/configurations/AppConfiguration.java
@@ -10,30 +10,23 @@ import org.springframework.context.annotation.Bean;
 
 public class AppConfiguration {
 
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(getHttpConnector());
-		return tomcat;
-	}
-
-	private Connector getHttpConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(7071);
-		// connector.setSecure(false); //connector.setRedirectPort(port);
-		return connector;
-	}
+	/*
+	 * @Bean public ServletWebServerFactory servletContainer() {
+	 * TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+	 * 
+	 * @Override protected void postProcessContext(Context context) {
+	 * SecurityConstraint securityConstraint = new SecurityConstraint();
+	 * securityConstraint.setUserConstraint("CONFIDENTIAL"); SecurityCollection
+	 * collection = new SecurityCollection(); collection.addPattern("/*");
+	 * securityConstraint.addCollection(collection);
+	 * context.addConstraint(securityConstraint); } };
+	 * tomcat.addAdditionalTomcatConnectors(getHttpConnector()); return tomcat; }
+	 * 
+	 * private Connector getHttpConnector() { Connector connector = new
+	 * Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+	 * connector.setScheme("http"); connector.setPort(7071); //
+	 * connector.setSecure(false); //connector.setRedirectPort(port); return
+	 * connector; }
+	 */
 
 }
